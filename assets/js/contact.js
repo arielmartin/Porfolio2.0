2c02652c34872138bfd9fda8f824c2da6871e97e
Ariel Martin <arielmartin80@gmail.com>
2021-09-09 12:03:54 -0300

    Initial commit

---
+++ assets/js/contact.js
@@ -0,0 +1,38 @@
+var elements = document.getElementById('test');
+var lang = elements.getAttribute('lang');
+
+
+//Formulario de contacto
+var action = "https://docs.google.com/forms/u/1/d/e/1FAIpQLSenEQOKGiv8xJS-Pu7Tj_AetWqLqbSnvtW753eEddcE79ZFFw/formResponse";
+var name = "entry.1440274532";
+var emailAddress = "emailAddress";
+var message = "entry.388888529";
+
+
+$(document).ready(function() {
+   $("#contact").attr("action",action);
+   $("#name").attr("name",name);
+   $("#emailAddress").attr("name",emailAddress);
+   $("#message").attr("name",message)
+});
+
+
+//links a redes
+var whatsapp = "https://api.whatsapp.com/send?phone=5491157673825&text=Ud se está contactando con Ariel Martin, Desarrollador Web. \"https://arielmartin.ml\"";
+var mailto = "mailto:arielmartin80@gmail.com";
+var github = "https://github.com/arielmartin80/";
+var gitlab = "https://gitlab.com/arielmartin80/";
+var linkedin = "https://www.linkedin.com/in/ariel-martin-705a3461";
+var tel ="tel:+5491157673825";
+
+$(document).ready(function() {
+    $(".linkedin").attr("href", linkedin);
+    $(".github").attr("href", github);
+    $(".gitlab").attr("href", gitlab);
+    $(".whatsapp").attr("href", whatsapp);
+    $(".mailto").attr("href", mailto)
+    $(".tel").attr("href", tel);
+ });
+ 
+
+ 
